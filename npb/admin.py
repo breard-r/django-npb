9b4ea0f7ecf3757cfea3a02984361799c6c6303d
Rodolphe Breard <rodolphe@what.tf>
2018-02-10 12:22:21 +0100

    Improve the paste admin interface

--- npb/admin.py
+++ npb/admin.py
@@ -10,8 +10,9 @@ class PasteAdmin(admin.ModelAdmin):
     ]
     list_filter = ['is_private', 'is_removed']
     search_fields = ['author_ip', 'lexer', 'title', 'content']
+    readonly_fields = ('author', 'author_ip', 'created_on', 'edited_on')
     fieldsets = [
-        (_('Meta'), {'fields': ['is_private', 'author', 'expire_on']}),
+        (_('Meta'), {'fields': ['is_private', 'author', 'author_ip', 'created_on', 'edited_on', 'expire_on']}),
         (_('Paste'), {'fields': ['lexer', 'title', 'content']}),
         (_('Administration'), {'fields': ['is_removed', 'removal_reason']}),
     ]
