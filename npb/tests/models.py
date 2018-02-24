747d3bbe9a9f2202631e2d16fb0934c6f6e6d6c1
Rodolphe Breard <rodolphe@what.tf>
2018-02-24 13:51:46 +0100

    Divide tests into several files

--- npb/tests/models.py
+++ npb/tests/models.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 from django.conf import settings
 from django.test import TestCase
-from .models import Paste, Report
+from ..models import Paste, Report
 import datetime
 
 
