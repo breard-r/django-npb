4648c7c725c08faa2329ea6515ab1d680f955369
Rodolphe Breard <rodolphe@what.tf>
2018-02-24 11:12:04 +0100

    Add a test for edition tolerance

--- npb/tests.py
+++ npb/tests.py
@@ -1,3 +1,26 @@
+from django.utils import timezone
+from django.conf import settings
 from django.test import TestCase
+from .models import Paste, Report
+import datetime
 
-# Create your tests here.
+
+class PasteModelTests(TestCase):
+    def test_edited(self):
+        now = timezone.now()
+        p = Paste(created_on=now, edited_on=now)
+
+        settings.NPB_EDIT_TOLERANCE = -1
+        self.assertIs(p.edited(), True)
+
+        settings.NPB_EDIT_TOLERANCE = 1
+        self.assertIs(p.edited(), False)
+
+        p.edited_on = now + datetime.timedelta(seconds=1)
+        self.assertIs(p.edited(), False)
+
+        p.edited_on = now + datetime.timedelta(seconds=2)
+        self.assertIs(p.edited(), True)
+
+        settings.NPB_EDIT_TOLERANCE = 10
+        self.assertIs(p.edited(), False)
